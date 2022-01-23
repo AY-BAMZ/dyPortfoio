dfa5169c1db2908613fe9790f941637d7b3a02ff
AY-BAMZ <aybamz181@gmail.com>
2022-01-23 22:37:01 +0100

    done

--- script.js
+++ script.js
@@ -36,3 +36,32 @@ window.smoothScroll = function(target) {
   // start scrolling
   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var fade = document.getElementsByClassName("fade");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+ 
+  slides[slideIndex-1].style.display = "block";
+  fade[slideIndex-1].style.opacity = 1;  
+  
+}
